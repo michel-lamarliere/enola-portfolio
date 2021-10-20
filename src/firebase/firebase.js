4f301e7906c59bac9bbd6a4015a7472dbb93490e
Michel Lamarlière <lamarliere.michel@icloud.com>
2021-10-20 13:15:04 +0200

    vercel deployment test

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: 'AIzaSyCniVfWt6VM8LfejRI1EuWqdhqv335uiY8',
+    authDomain: "mon-portfolio-e9d2a.firebaseapp.com",
+    projectId: "mon-portfolio-e9d2a",
+    storageBucket: "mon-portfolio-e9d2a.appspot.com",
+    messagingSenderId: "1039741393766",
+    appId: "1:1039741393766:web:35bc023f2de36fa3e946a7",
+    measurementId: "G-DP50S7DCJE"
+});
+
+const db = firebaseApp.firestore();
+
+export default db ;
+
