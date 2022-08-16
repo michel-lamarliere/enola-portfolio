be23e19cfd5cacdcc1c8f75f6b40ca04441bd8f6
Michel Lamarlière <m.lamarliere@icloud.com>
2022-08-16 17:03:15 +0200

    fix: testing vercel fix, fixed a mobile Home > Main problem

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,11 +4,9 @@ import Main from "./components/Main";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 
-import classes from "./styles.module.scss";
-
 const Home: React.FC = () => {
   return (
-    <div className={classes.wrapper}>
+    <div>
       <Main />
       <Services />
       <Contact />
